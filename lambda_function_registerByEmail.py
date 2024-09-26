347994ece66b6e616cebc6d571cf5875de8bd876
clownUR <ruka.uesugi11055011@icloud.com>
2024-09-26 18:30:27 +0900

    登録APIの実装に関するプログラムの修正

--- lambda_function_registerByEmail.py
+++ lambda_function_registerByEmail.py
@@ -2,38 +2,35 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-    # TODO implement
-  email = event.get('email')
+  '''
+  メールアドレスでの登録APIの実装
+  '''
+
+  user_email = event.get('email')
   password = event.get('password')
 
-  if not (email and password):
+  if not (user_email and password):
     return {
       'status code' : 400,
       'message': 'failed to create user'
     }
 
   cognito_idp_client = boto3.client('cognito-idp')
-  #cognitoのclientID
-  client_id = 'your_clinet_id'
 
   try:
-    #sign_upに変える
-    response = cognito_idp_client.sign_up(
-      ClientId = client_id,
-      Username = email,
-      Password = password,
-      UserAttributes=[
-        {
-          'Name': 'email',
-          'Value': email
-        },
-      ]
-    )
+    kwargs = {
+      #cognitoのclientID
+      'ClientId' : 'your_client_id',
+      'Username' : user_email,
+      'Password' : password,
+      'UserAttributes' : [{'Name': 'email', 'Value': user_email}]
+    }
 
-    jwt = response['AuthenticationResult']['IdToken']
+    response = cognito_idp_client.sign_up(**kwargs)
+
     return {
       'statuscode' : 200,
-      'session' : jwt,
+      'session' : 'jwt',
       'message' : 'complete create user'
     }
 
